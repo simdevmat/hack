0f9c969b3a5642934a6d20a371c77c7f046db7e2
Simon Matthews <simdevmat@gmail.com>
2016-11-26 12:33:25 +1300

    Added dynamic loading of google cloud pricing page and nice ouput

--- get_gcloud_pricing.rb
+++ get_gcloud_pricing.rb
@@ -3,14 +3,15 @@ require 'nokogiri'
 
 
 MACHINE_TYPE=0
-VIRTUAL_CPUS=1
+VCPUS=1
 MEMORY=2
 PRICE=3
 PREEMPTIBLE_PRICE=4
 CUSTOM_PRICE=1
 CUSTOM_PREEMPTIBLE_PRICE=2
-#page = Nokogiri::HTML("https://cloud.google.com/compute/pricing"))   
-page = Nokogiri::HTML(File.read('google.html'))
+data = RestClient.get("https://cloud.google.com/compute/pricing")
+File.open('google.html', 'w') { |file| file.write(data)}
+page = Nokogiri::HTML(data)
 
 
 custom_types = {}
@@ -22,40 +23,43 @@ page.css('div table').each do |div|
     div.css('tbody tr').each do |type|
       if type.css('td').size == 5
         machine_type = type.css('td')[MACHINE_TYPE].text.gsub(/[\n ]/, "")
-        vcpus        = type.css('td')[VIRTUAL_CPUS].text
-        memory       = type.css('td')[MEMORY].text.sub('GB', '')
-        preemptible_price = {}
-        standard_price = {}
+        predefined_types[machine_type] = {}
         type.css('td')[PRICE].keys.select { |prices| prices.include?('hourly')}.each do |region|
-          standard_price[region] = type.css('td')[PRICE][region].sub('$', '')
+          predefined_types[machine_type][region] = { 
+            "vcpus"             => type.css('td')[VCPUS][region] || type.css('td')[VCPUS].text,
+            "memory"            => (type.css('td')[MEMORY][region] || type.css('td')[MEMORY].text).sub('GB', ''),
+            "standard_price"    => type.css('td')[PRICE][region].sub('$', ''),
+            "preemptible_price" => type.css('td')[PREEMPTIBLE_PRICE][region].sub('$', ''),
+          } 
         end
-        type.css('td')[PREEMPTIBLE_PRICE].keys.select { |prices| prices.include?('hourly')}.each do |region|
-          preemptible_price[region] = type.css('td')[PREEMPTIBLE_PRICE][region].sub('$', '')
-        end
-        predefined_types[machine_type] = {
-          "vcpus" => vcpus,
-          "memory" => memory,
-          "standard_prices" => standard_price,
-          "preemptible_prices" => preemptible_price,
-        } 
       end
     end
   elsif table[MACHINE_TYPE].text == 'Item' and table.size == 3
     div.css('tbody tr').select { |t| t.css('td')[0].text == 'vCPU' or t.css('td')[0].text == 'Memory' }.each do |type|
       name = type.css('td')[0].text.downcase
-      custom_types[name] = {
-        "standard_prices" => {},
-        "preemptible_prices" => {},
-      }
+      custom_types[name] = {}
       type.css('td')[CUSTOM_PRICE].keys.select { |prices| prices.include?('hourly')}.each do |region|
-        custom_types[name]['standard_prices'][region] = type.css('td')[CUSTOM_PRICE][region].gsub('$', '').gsub(/[\$\s\/]*(vCPU|GB)/, '')
-      end
-      type.css('td')[CUSTOM_PREEMPTIBLE_PRICE].keys.select { |prices| prices.include?('hourly')}.each do |region|
-        custom_types[name]['preemptible_prices'][region] = type.css('td')[CUSTOM_PREEMPTIBLE_PRICE][region].sub('$', '').gsub(/[\$\s\/]*(vCPU|GB)/, '')
+        custom_types[name][region] = {
+         'standard_price'     => type.css('td')[CUSTOM_PRICE][region].gsub('$', '').gsub(/[\$\s\/]*(vCPU|GB)/, ''),
+         'preemptible_price' => type.css('td')[CUSTOM_PREEMPTIBLE_PRICE][region].sub('$', '').gsub(/[\$\s\/]*(vCPU|GB)/, '')
+        }
       end
     end
   end
 end
 
-puts custom_types
-puts predefined_types
+
+predefined_types.each do |name, regions|
+puts name.upcase
+  regions.each do |region, data|
+    puts "\t#{region.sub(/-.*/, '')}: #{data['vcpus']} #{data['memory']} #{data['standard_price']} #{data['preemptible_price']}"
+  end
+end
+
+custom_types.each do |name, regions|
+  puts "Custom #{name.upcase}"
+  regions.each do |region, data|
+    puts "\t#{region.sub(/-.*/, '')}: #{data['standard_price']} #{data['preemptible_price']}"
+  end
+end
+    
